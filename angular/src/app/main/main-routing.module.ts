ac58a1731faa715d902c25d6c97af90a5f187b12
vietKlqmw <113014603+vietKlqmw@users.noreply.github.com>
2023-12-01 01:37:08 +0700

    update be

--- angular/src/app/main/main-routing.module.ts
+++ angular/src/app/main/main-routing.module.ts
@@ -13,11 +13,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 //MASTER
                     //Common
                     {
-                        path: 'master/common/vehicle-cbu',
-                        loadChildren: () => import('./master/common/vehicle-cbu/vehicle-cbu.module').then(m => m.VehicleCbuModule),
-                        //data: { permission: 'Pages.Master.Cmm.VehicleCBU.View' }
-                    },
-                    {
                         path: 'master/common/container-status',
                         loadChildren: () => import('./master/common/container-status/container-status.module').then(m => m.ContainerStatusModule),
                         //data: { permission: 'Pages.Master.Cmm.VehicleCBU.View' }
@@ -85,21 +80,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
                     {
                         path: 'master/common/car-series',
                         loadChildren: () => import('./master/common/car-series/car-series.module').then(m => m.CarSeriesModule),
-                        //data: { permission: 'Pages.Master.Cmm.VehicleCBU.View' }
-                    },
-                    {
-                        path: 'master/common/engine-type',
-                        loadChildren: () => import('./master/common/engine-type/engine-type.module').then(m => m.EngineTypeModule),
-                        //data: { permission: 'Pages.Master.Cmm.VehicleCBU.View' }
-                    },
-                    {
-                        path: 'master/common/engine-model',
-                        loadChildren: () => import('./master/common/engine-model/engine-model.module').then(m => m.EngineModelModule),
-                        //data: { permission: 'Pages.Master.Cmm.VehicleCBU.View' }
-                    },
-                    {
-                        path: 'master/common/transmission-type',
-                        loadChildren: () => import('./master/common/transmission-type/transmission-type.module').then(m => m.TransmissionTypeModule),
                         //data: { permission: 'Pages.Master.Cmm.VehicleCBU.View' }
                     },
 //MANAGEMENT
--- angular/src/app/shared/layout/nav/app-navigation.service.ts
+++ angular/src/app/shared/layout/nav/app-navigation.service.ts
@@ -36,15 +36,10 @@ export class AppNavigationService {
                     new AppMenuItem('Fuel Type', '', 'flaticon-car', '/app/main/master/common/fuel-type'),
                     new AppMenuItem('Carfamily', '', 'flaticon-car', '/app/main/master/common/carfamily'),
                     new AppMenuItem('Car Series', '', 'flaticon-car', '/app/main/master/common/car-series'),
-                    new AppMenuItem('Engine Model', '', 'flaticon-car', '/app/main/master/common/engine-model'),
-                    new AppMenuItem('Engine Type', '', 'flaticon-car', '/app/main/master/common/engine-type'),
-                    new AppMenuItem('Transmission Type', '', 'flaticon-car', '/app/main/master/common/transmission-type'),
                 ]),
 
                 new AppMenuItem('Material', '', 'flaticon-car', '/app/main/master/common/material'),
-                // new AppMenuItem('Engine', '', 'flaticon-car', '/app/main/master/common/'),
                 // new AppMenuItem('Vehicle CKD', '', 'flaticon-car', '/app/main/master/common/vehicle-ckd'),
-                //new AppMenuItem('Vehicle CBU', '', 'flaticon-car', '/app/main/master/common/vehicle-cbu'),
             ]),
 //MAIN
             new AppMenuItem('MANAGEMENT', '', 'flaticon-user-settings', '',[],[
